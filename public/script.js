ced4131b7c6fda041b14418b07d5f36bd9c502fe
pacy001 <pacifiquemurangwa001@gmail.com>
2022-06-06 10:18:30 +0200

    finished implementation of socket.io on client-side

--- public/script.js
+++ public/script.js
@@ -1,11 +1,18 @@
 let myVideoStream;
 const videoGrid = document.getElementById("video-grid");
+const socket = io('/');
+socket.on("connect", () => {
+    console.log(socket.id);
+  });
+
 const myVideo = document.createElement("video");
 myVideo.muted = true;
+
 navigator.mediaDevices.getUserMedia({
     audio: true,
     video: true,
 })
+
 .then((stream) => {
     myVideoStream = stream;
     addVideoStream(myVideo, stream);
@@ -13,7 +20,6 @@ navigator.mediaDevices.getUserMedia({
 
 const addVideoStream = (video, stream) => {
     video.srcObject = stream;
-    console.log(stream);
     video.addEventListener("loadedmetadata", () => {
 
        video.play();
