9f6c599f48ffaefbd3d2e49d2fa7798516050103
pacy001 <pacifiquemurangwa001@gmail.com>
2022-06-06 11:42:42 +0200

    finished server-side functionalities

--- server.js
+++ server.js
@@ -1,36 +1,28 @@
-const { v4: uuidv4 } = require("uuid");
-const colors = require('colors');
-const ejs = require('ejs');
-const dotenv = require('dotenv').config({path: './config/.env'});
-const express = require('express');
+const express = require("express");
 const app = express();
-const server = require('http').Server(app);
+const server = require("http").Server(app);
+const { v4: uuidv4 } = require("uuid");
+const dotenv = require("dotenv").config({path: './config/.env'});
+app.set("view engine", "ejs");
 const io = require("socket.io")(server);
-const { ExpressPeerServer } = require('peer');
-
-
+const { ExpressPeerServer } = require("peer");
 const peerServer = ExpressPeerServer(server, {
-    debug: true,
-    path: '/peerServer'
+debug: true,
 });
-
-app.use(express.static('public'));
-app.set('view engine', 'ejs');
-
-
-io.on('connection', (socket) => {
-    console.log(socket);
+app.use("/peerjs", peerServer);
+app.use(express.static("public"));
+app.get("/", (req, res) => {
+res.redirect(`/${uuidv4()}`);
 });
-
-app.get('/', (req, res) => {
-   res.redirect(`/${uuidv4()}`);
-});
-
 app.get("/:room", (req, res) => {
-    res.render('room', { roomId: req.param.room });
+res.render("room", { roomId: req.param.room });
 });
-
-
+io.on("connection", (socket) => {
+socket.on("join-room", (roomId, userId) => {
+socket.join(roomId);
+socket.to(roomId).broadcast.emit("user-connected", userId);
+});
+});
 server.listen(process.env.PORT, () => {
-    console.log(`server is running at ${process.env.PORT}`.blue.bold.bgWhite);
+    console.log(`server is at ${process.env.PORT}`)
 });
