35b15ced2cd1aab70bacd6532676e3f7fb75c658
pacy001 <pacifiquemurangwa001@gmail.com>
2022-06-06 11:40:10 +0200

    finished room.ejs

--- public/script.js
+++ public/script.js
@@ -1,28 +1,56 @@
-let myVideoStream;
+const socket = io("/");
 const videoGrid = document.getElementById("video-grid");
-const socket = io('/');
-socket.on("connect", () => {
-    console.log(socket.id);
-  });
-
 const myVideo = document.createElement("video");
 myVideo.muted = true;
 
-navigator.mediaDevices.getUserMedia({
+var peer = new Peer(undefined, {
+    path: "/peerjs",
+    host: "/",
+    port: "1010",
+});
+
+let myVideoStream;
+
+navigator.mediaDevices
+.getUserMedia({
     audio: true,
     video: true,
 })
-
 .then((stream) => {
+
     myVideoStream = stream;
     addVideoStream(myVideo, stream);
+    peer.on("call", (call) => {
+        call.answer(stream);
+        const video = document.createElement("video");
+        call.on("stream", (userVideoStream) => {
+        addVideoStream(video, userVideoStream);
+    });
 });
 
-const addVideoStream = (video, stream) => {
-    video.srcObject = stream;
-    video.addEventListener("loadedmetadata", () => {
+socket.on("user-connected", (userId) => {
+    connectToNewUser(userId, stream);
+});
 
-       video.play();
-       videoGrid.appendChild(video);
+});
+
+    const connectToNewUser = (userId, stream) => {
+        const call = peer.call(userId, stream);
+        const video = document.createElement("video");
+        call.on("stream", (userVideoStream) => {
+        addVideoStream(video, userVideoStream);
+    });
+};
+
+peer.on("open", (id) => {
+    socket.emit("join-room", ROOM_ID, id);
+});
+
+    //append video to the video grid
+    const addVideoStream = (video, stream) => {
+        video.srcObject = stream;
+        video.addEventListener("loadedmetadata", () => {
+        video.play();
+        videoGrid.append(video);
     });
 };
