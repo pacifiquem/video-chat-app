a32cc0123f97331d558b52ea09d01041000a44f3
pacy001 <pacifiquemurangwa001@gmail.com>
2022-06-06 08:55:29 +0200

    configurations of peer server finished

--- server.js
+++ server.js
@@ -1,27 +1,31 @@
-const express = require('express');
 const { v4: uuidv4 } = require("uuid");
 const colors = require('colors');
+const ejs = require('ejs');
 const dotenv = require('dotenv').config({path: './config/.env'});
+const express = require('express');
 const app = express();
 const server = require('http').Server(app);
-const ejs = require('ejs');
 const io = require("socket.io")(server);
-const { ExpressPeerServer } = require("peer");
+const { ExpressPeerServer } = require('peer');
+
+
 const peerServer = ExpressPeerServer(server, {
     debug: true,
+    path: '/peerServer'
 });
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
+
 
 app.get('/', (req, res) => {
    res.redirect(`/${uuidv4()}`);
 });
 
 app.get("/:room", (req, res) => {
-
     res.render('room', { roomId: req.param.room });
 });
+
 
 server.listen(process.env.PORT, () => {
     console.log(`server is running at ${process.env.PORT}`.blue.bold.bgWhite);
